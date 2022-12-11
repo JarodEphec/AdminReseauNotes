3d980ece9579ce07883488661899f784e462af58
AlexandreDoneux <94830560+AlexandreDoneux@users.noreply.github.com>
2022-12-11 23:28:10 +0100

    Update addUsers.ps1

--- addUsers.ps1
+++ addUsers.ps1
@@ -11,31 +11,41 @@ write-host $users
 # Boucle qui, pour chque utilisateur défini dans le csv ....
 foreach($user in $users){
 
-# On récupère les différentes données, arrive pas à accéder
-$username = $user.username
-$firstname = $user.firstname
-$lastname = $user.lastname
-$password = $user.password
+    # On récupère les différentes données, arrive pas à accéder
+    $username = $user.username
+    $firstname = $user.firstname
+    $lastname = $user.lastname
+    $password = $user.password
 
-# .... à compléter si besoin
-
-
+    # .... à compléter si besoin
 
 
-# On vérifie si il n'y a pas déjà un utilisateur existant aevc ce nom
-if(Get-ADUser -Filter {samAccountName -eq $username}){
-    Write-Host "Il existe déjà unutilisateur $username"
-}
-else{
 
-#Attention, -Name doit être en première position
 
-New-ADUser `
-    -Name "$lastname $firstname" `    -SamAccountName $username `    -GivenName $firstname `    -SurName $lastname `    -Enabled $true `    -DisplayName "$lastname $firstname" `    -Path "OU=utilisateurs, DC=l1-3, DC=lab" `    -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) `    -PasswordNeverExpires $true `    -ChangePasswordAtLogon $false    
-Write-Host "L'utilisateur $username a été créé."
+    # On vérifie si il n'y a pas déjà un utilisateur existant aevc ce nom
+    if(Get-ADUser -Filter {samAccountName -eq $username}){
+        Write-Host "Il existe déjà unutilisateur $username"
+    }
+    else{
 
-}
-# à rajouter ?
-#-UserPrincipalName "$username@<domain>" `
+        #Attention, -Name doit être en première position
+
+        New-ADUser `
+            -Name "$lastname $firstname" `
+            -SamAccountName $username `
+            -GivenName $firstname `
+            -SurName $lastname `
+            -Enabled $true `
+            -DisplayName "$lastname $firstname" `
+            -Path "OU=utilisateurs, DC=l1-3, DC=lab" `
+            -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) `
+            -PasswordNeverExpires $true `
+            -ChangePasswordAtLogon $false
+
+        Write-Host "L'utilisateur $username a été créé."
+
+    }
+    # à rajouter ?
+    #-UserPrincipalName "$username@<domain>" `
 
 }
