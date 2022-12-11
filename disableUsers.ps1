4961d9fbdb276e137fb750b62191c2e160fde55b
AlexandreDoneux <94830560+AlexandreDoneux@users.noreply.github.com>
2022-12-12 00:19:06 +0100

    Update disableUsers.ps1

--- disableUsers.ps1
+++ disableUsers.ps1
@@ -8,23 +8,23 @@ $users = Import-Csv -Path $args[0] -Header "username"
 
 foreach($user in $users){
 
-# On récupère les différentes données
-$username = $user.username
+    # On rÃ©cupÃ¨re les diffÃ©rentes donnÃ©es
+    $username = $user.username
 
-# .... à compléter si besoin
-
-
-# On vérifie si il y a un utilisateur existant aevc ce nom. ATTENTION : ne vérifie pas si le compte est déjà désactivé
-if(Get-ADUser -Filter {samAccountName -eq $username} -SearchBase "OU=utilisateurs,DC=l1-3,DC=lab"){
-    Disable-ADAccount $username
+    # .... Ã  complÃ©ter si besoin
 
 
-    Write-Host "Compte $username désactivé."
-}
-# Si l'utilisateur n'existe pas on préviens.
-else{
-    Write-Host "L'utilisateur $username n'existe pas."
+    # On vÃ©rifie si il y a un utilisateur existant aevc ce nom. ATTENTION : ne vÃ©rifie pas si le compte est dÃ©jÃ  dÃ©sactivÃ©
+    if(Get-ADUser -Filter {samAccountName -eq $username} -SearchBase "OU=utilisateurs,DC=l1-3,DC=lab"){
+        Disable-ADAccount $username
 
-}`
+
+        Write-Host "Compte $username dÃ©sactivÃ©."
+    }
+    # Si l'utilisateur n'existe pas on prÃ©viens.
+    else{
+        Write-Host "L'utilisateur $username n'existe pas."
+
+    }
 
 }
